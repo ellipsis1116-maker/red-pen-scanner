9680e8e0edcbb621f6120d65ef381ab84f27d6fc
ellipsis1116-maker <kellipsis1116@gmail.com>
2025-08-30 17:16:08 +0800

    Update camera.js

--- src/camera.js
+++ src/camera.js
@@ -1,20 +1,13 @@
 let currentStream = null;
-
 const videoEl = document.getElementById('video');
 
 async function initCamera({ facingMode='environment', width=1280, height=720 } = {}) {
-  if (currentStream) {
-    currentStream.getTracks().forEach(t => t.stop());
-  }
-  const constraints = {
-    audio: false,
-    video: { width: { ideal: width }, height: { ideal: height }, facingMode }
-  };
+  if (currentStream) currentStream.getTracks().forEach(t => t.stop());
+  const constraints = { audio: false, video: { width: { ideal: width }, height: { ideal: height }, facingMode } };
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
   currentStream = stream;
   videoEl.srcObject = stream;
   await videoEl.play().catch(()=>{});
   return stream;
 }
-
 export { initCamera };
