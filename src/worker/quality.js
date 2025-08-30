2d9f284e1aac5a9dc6cfb3a8f7f1836c64e45e0c
ellipsis1116-maker <kellipsis1116@gmail.com>
2025-08-30 16:13:51 +0800

    Update quality.js

--- src/worker/quality.js
+++ src/worker/quality.js
@@ -3,7 +3,7 @@ export function estimateQuality(mask, w, h) {
   for (let i=0;i<mask.length;i++) if (mask[i]) red++;
   const redRatio = red / (w*h);
   const suggestions = [];
-  if (redRatio < 0.001) suggestions.push('移动设备靠近试卷');
-  if (redRatio < 0.0003) suggestions.push('光线不足，请打开闪光灯');
+  if (redRatio < 0.001) suggestions.push('靠近试卷');
+  if (redRatio < 0.0003) suggestions.push('光线不足');
   return { redRatio, suggestions };
 }
